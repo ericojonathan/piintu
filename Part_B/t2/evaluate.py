0be425e8db4f9fc1e229237bcffa615d33909634
Eric O. Jonathan <ericojonathan@users.noreply.github.com>
2021-04-04 11:57:15 +0700

    Create evaluate.py

---
+++ Part_B/t2/evaluate.py
@@ -0,0 +1,38 @@
+def evaluate(string):
+    n = ''
+    total = 0
+
+    for i in range(0, len(string)):
+        s = string[i]
+        op = ['-','+',' ']
+
+        if s.isnumeric() == False and s not in op:
+            raise Exception("string cannot be evaluated")
+            return 0
+
+        if s.strip() == '':
+            continue
+        n += s
+        if s.isnumeric():
+            #following the example of 1+1+1-1 = 3
+            #else, remove this if conditional
+            if len(n) == 2:
+                if n[0] =='-':
+                    n = ''
+                    continue
+            total += int(n)
+            n = ''
+    return total
+
+def testEvaluate():
+    strings = [
+        '1+1+1-1',
+        '-1+2+3-1',
+        '+2+4',
+        '5 + 5',
+        '1*2'
+    ]
+    for s in strings:
+        print(evaluate(s))
+
+testEvaluate()
